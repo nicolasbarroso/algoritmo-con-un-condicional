8a6f53215df4da14f6ce30f6c7548d1466149b10
Nico Barroso <team@platzi.com>
2021-08-01 20:35:39 -0300

    segundo

---
+++ miarchivo.js
@@ -0,0 +1,7 @@
+const ingresaTexto = prompt("Escribí la palabra Hola");
+        if (ingresaTexto == "Hola"){
+            console.log("Ingresaste Hola :)");
+        }
+        else{
+            alert("No ingresaste Hola :(")
+        }
